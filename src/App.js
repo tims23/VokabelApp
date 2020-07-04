235151d5066d11d395e090d66fef20c804cc3971
tims23 <niklas.sassmannshausen@gmx.de>
2020-07-04 19:27:36 +0200

    tested firebase

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import Haus from "./Haus";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Haus from './Haus';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Haus />
-        <p>Soos</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<p>Test für firebase</p>
+				<Haus />
+				<p>Soos</p>
+				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
