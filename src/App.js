9cef5fca0db0d7ee1709646db25d27148fe9854a
tims23 <niklas.sassmannshausen@gmx.de>
2020-07-04 18:50:55 +0200

    soos

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
 					Edit <code>src/App.js</code> and save to reload.
 				</p>
 				<Haus />
+				<p>Soos</p>
 				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
 					Learn React
 				</a>
