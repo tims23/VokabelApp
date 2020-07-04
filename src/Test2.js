420e7a37b120d488884ddc4eb1ed5401e5c2b8d3
tims23 <tims23@users.noreply.github.com>
2020-07-04 17:07:41 +0000

    Prettified Code!

--- src/Test2.js
+++ src/Test2.js
@@ -1,9 +1,9 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 export class Test2 extends Component {
-	render() {
-		return <div>Test</div>;
-	}
+  render() {
+    return <div>Test</div>;
+  }
 }
 
 export default Test2;
