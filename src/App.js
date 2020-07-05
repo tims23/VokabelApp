6ac609bcacc84792ae12c13b5f23ddfde0ccb781
tims23 <tims23@users.noreply.github.com>
2020-07-05 10:02:58 +0000

    Prettified Code!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Haus from './Haus';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Haus from "./Haus";
 
 function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<p>Test 11 für firebase</p>
-				<Haus />
-				<p>Soos</p>
-				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-					Learn React
-				</a>
-			</header>
-		</div>
-	);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>Test 11 für firebase</p>
+        <Haus />
+        <p>Soos</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
