cb0c0e87526eaf1c23fabaeadb5c701b2d2ab2ee
tims23 <niklas.sassmannshausen@gmx.de>
2020-07-05 12:28:13 +0200

    branches test

--- src/Haus.js
+++ src/Haus.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 
 export class Haus extends Component {
 	render() {
-		return <div>Branches Test</div>;
+		return <div>Funktionieren die Branches?</div>;
 	}
 }
 
