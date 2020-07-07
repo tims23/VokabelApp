4f672d9eb393921844dd6c69d665812f54131ffa
tims23 <niklas.sassmannshausen@gmx.de>
2020-07-07 16:57:11 +0200

    testing firestore

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+	const burritoRef = useFirestore().collection('tryreactfire').doc('burrito');
+
+	const burrito = useFirestoreDocData(burritoRef);
+
 	return (
 		<div className="App">
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
 				<p>Eine leere WebApp</p>
+				<p>The burrito is {burrito.yummy ? 'good' : 'bad'}!</p>
 			</header>
 		</div>
 	);
