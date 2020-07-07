868c745fac797c07bba3aa1822a8a908b77cacb5
tims23 <niklas.sassmannshausen@gmx.de>
2020-07-07 17:54:35 +0200

    firestore tested

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {SuspenseWithPerf} from 'reactfire';
+import Burrito from './Burrito';
 
 function App() {
-	const burritoRef = useFirestore().collection('tryreactfire').doc('burrito');
-
-	const burrito = useFirestoreDocData(burritoRef);
-
 	return (
 		<div className="App">
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
 				<p>Eine leere WebApp</p>
-				<p>The burrito is {burrito.yummy ? 'good' : 'bad'}!</p>
+				<SuspenseWithPerf fallback={<p>loading burrito status...</p>} traceId={'load-burrito-status'}>
+					<Burrito />
+				</SuspenseWithPerf>
 			</header>
 		</div>
 	);
