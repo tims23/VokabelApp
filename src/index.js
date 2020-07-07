a0b8ab27ff375dc0e1b3da2bd8c19e25caa22d47
tims23 <niklas.sassmannshausen@gmx.de>
2020-07-07 16:54:00 +0200

    added firebase App Provider from reactfire

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import {FirebaseAppProvider} from 'reactfire';
+
+const firebaseConfig = {
+	apiKey: 'AIzaSyBPDYXKqmtZyD9e8_W0N98z_2lgdSSh970',
+	authDomain: 'vokabelapp-170f6.firebaseapp.com',
+	databaseURL: 'https://vokabelapp-170f6.firebaseio.com',
+	projectId: 'vokabelapp-170f6',
+	storageBucket: 'vokabelapp-170f6.appspot.com',
+	messagingSenderId: '624533432759',
+	appId: '1:624533432759:web:19a551ee21aafd92636032',
+	measurementId: 'G-R80Y8N1NRV'
+};
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+	<React.StrictMode>
+		<FirebaseAppProvider firebaseConfig={firebaseConfig}>
+			<App />
+		</FirebaseAppProvider>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
