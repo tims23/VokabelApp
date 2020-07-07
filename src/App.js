0499bac625cfe4e52855fcacfef5f18e46911b9b
tims23 <niklas.sassmannshausen@gmx.de>
2020-07-07 16:32:00 +0200

    tested CI angain

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
 		<div className="App">
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
-				<p>Eine leere App</p>
+				<p>Eine leere WebApp</p>
 			</header>
 		</div>
 	);
