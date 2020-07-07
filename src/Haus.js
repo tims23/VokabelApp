7ef8dbfcceea82e418c33f9f8969166e69eff73f
tims23 <niklas.sassmannshausen@gmx.de>
2020-07-07 16:10:14 +0200

    removed files for testing

--- src/Haus.js
+++
@@ -1,9 +0,0 @@
-import React, {Component} from 'react';
-
-export class Haus extends Component {
-	render() {
-		return <div>Funktionieren die Branches?</div>;
-	}
-}
-
-export default Haus;
--- src/Test2.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-export class Test2 extends Component {
-  render() {
-    return <div>Test</div>;
-  }
-}
-
-export default Test2;
