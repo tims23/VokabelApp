8b610d91bcde9b3ed1256384d5845a6f2a2c7547
tims23 <niklas.sassmannshausen@gmx.de>
2020-07-08 22:38:26 +0200

    tried sth

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
 		<div className="App">
 			<Router>
 				<Switch>
-					<Route path="/header">
+					<Route path="">
 						<header className="App-header">
 							<img src={logo} className="App-logo" alt="logo" />
 							<p>Eine leere WebApp</p>
